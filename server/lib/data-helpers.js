3b58b043d4e5c6a4ee1348e033bd9387182c9027
JoshLampen <joshlampen2@gmail.com>
2020-05-20 00:11:00 +0000

    Changed property name

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -18,7 +18,7 @@ module.exports = function makeDataHelpers(db) {
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
       simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+        const sortNewestFirst = (a, b) => a.dateCreated - b.dateCreated;
         callback(null, db.tweets.sort(sortNewestFirst));
       });
     }
