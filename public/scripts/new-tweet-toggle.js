e50d04823eb4553c94acf1c3b1c774c8778e9346
JoshLampen <joshlampen2@gmail.com>
2020-05-21 22:36:27 +0000

    Arrow direction in nav button now changes based on if new tweet section is visible or not

--- public/scripts/new-tweet-toggle.js
+++ public/scripts/new-tweet-toggle.js
@@ -1,17 +1,24 @@
 $(document).ready(function() {
   const button = $(".nav-button");
+  const upIcon = button.find("div").find(".fa-chevron-up");
+  const downIcon = button.find("div").find(".fa-chevron-down");
   const newTweetSection = $(".new-tweet");
   const textarea = newTweetSection.find("#tweet-text");
 
   newTweetSection.hide();
+  upIcon.hide();
 
   button.click(function() {
     if (newTweetSection.is(":hidden")) {
+      downIcon.hide();
+      upIcon.show();
       newTweetSection.slideDown(function() {
         textarea.focus();
       });
       button.blur();
     } else {
+      upIcon.hide();
+      downIcon.show();
       newTweetSection.slideUp();
       button.blur();
     }
